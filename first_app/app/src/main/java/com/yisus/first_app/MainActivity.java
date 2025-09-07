b50efe8588c4e0d4a652d2fd4081e044dd5eb7d3
Yisus0x19 <jesuspenarrieta03@gmail.com>
2025-09-07 13:04:39 -0600

    first_app logic design

--- first_app/app/src/main/java/com/yisus/first_app/MainActivity.java
+++ first_app/app/src/main/java/com/yisus/first_app/MainActivity.java
@@ -2,6 +2,9 @@ package com.yisus.first_app;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNum1,editTextNum2;
+    private Button buttonSum;
+    private TextView textViewResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initComponents();
+        addListeners();
+
+    }
+    private void initComponents(){
+        editTextNum1=findViewById(R.id.eTNum1);
+        editTextNum2=findViewById(R.id.eTNum2);
+        textViewResult=findViewById(R.id.tvResult);
+        buttonSum=findViewById(R.id.btSum);
+    }
+
+    private void addListeners(){
+        buttonSum.setOnClickListener(v-> textViewResult.setText(calculateSum()));
+    }
+
+    public String calculateSum() {
+        try {
+            return "Resultado: " + (Integer.parseInt(editTextNum1.getText().toString()) + Integer.parseInt(editTextNum2.getText().toString()));
+
+        }catch (Exception e){
+            return "Error al realizar la suma, porfavor digite números solamente";
+        }
     }
 }
+
